b1b4a9a43b3ffab7bc4acb4d69bd62927d133a02
TheElectronWill <theelectronwill@gmail.com>
2017-09-17 00:20:24 +0200

    Add a deployment script to copy the utils scripts to the projects' dirs

---
+++ deploy_utils.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e # Stops the script on error
+
+copy_scripts_to() {
+	echo $1
+	cp scripts/* "$1/"
+}
+
+cd "$(dirname "$0")"
+ls scripts
+if [ $# -eq 0 ]; then
+	for dir in ../*/; do
+		if [[ $dir != "../common-utils/" ]]; then
+			copy_scripts_to "$dir"
+		fi
+	done
+else
+	for dir_name in $@; do
+		copy_scripts_to "../$dir_name"
+	done
+fi
