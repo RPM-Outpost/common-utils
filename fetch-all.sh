a57ee14270d56b94c689fb84e6a23ff9ea0255ae
TheElectronWill <theelectronwill@gmail.com>
2020-04-22 15:49:23 +0200

    Fix scripts dir

--- fetch-all.sh
+++ fetch-all.sh
@@ -3,6 +3,7 @@
 # Runs git fetch in all the directories inside the script directory
 
 cd "$(dirname "$0")"
+cd ..
 for dir in */; do
 	echo -e "\033[1mFetching $dir\033[0m"
 	cd $dir
@@ -10,4 +11,4 @@ for dir in */; do
 	cd ..
 done
 echo '----------'
-echo -e "\033[1;32mFetch completed."
+echo -e "\033[1;32mFetch completed.\033[0m"
